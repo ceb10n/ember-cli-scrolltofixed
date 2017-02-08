7e0d147f8dfaac5185d3a767959916860e527989
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2017-02-08 11:58:04 -0200

    added ref to template

--- addon/components/scroll-to-fixed.js
+++ addon/components/scroll-to-fixed.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
+import layout from '../templates/components/scroll-to-fixed';
 
 export default Ember.Component.extend({
+  layout,
   tagName: 'nav',
   classNames: ['scrollToFixed'],
-
 
   didInsertElement() {
     this._super(...arguments);
