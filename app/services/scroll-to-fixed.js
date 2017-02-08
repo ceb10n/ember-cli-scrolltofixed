6ed069bc49ecd761a9343ff9d91f337152188160
Rafael de Oliveira Marques <rafaelomarques@gmail.com>
2017-02-08 09:45:55 -0200

    First commit

---
+++ app/services/scroll-to-fixed.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-scrolltofixed/services/scroll-to-fixed';
--- index.js
+++ index.js
@@ -2,5 +2,16 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-scrolltofixed'
+  name: 'ember-cli-scrolltofixed',
+
+  included: function(app, parentAddon) {
+    var target = (parentAddon || app);
+
+    if (target.app) {
+      target = target.app;
+    }
+
+    var bowerDir = target.bowerDirectory;
+    target.import(bowerDir + '/ScrollToFixed/jquery-scrolltofixed-min.js');
+  }
 };
